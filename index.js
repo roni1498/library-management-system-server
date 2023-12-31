2ae886c86dd8dd7b614519ae8e96e0358e3f8a04
roni1498 <roni.bimt@gmail.com>
2023-12-31 12:19:23 +0600

    Implement JWT authentication for securing Borrow Book page
    This commit introduces JSON Web Token (JWT) authentication to secure the Borrow Book page. Users now need a valid JWT to access this page, enhancing the overall security of the application. The implementation includes server-side changes to verify JWT tokens and client-side updates to handle authentication and authorization for the Borrow Book feature.

--- index.js
+++ index.js
@@ -1,14 +1,21 @@
 const express = require("express");
 const cors = require("cors");
 const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5173'
+  ],
+  credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser())
 
 // mongodb
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vyh864y.mongodb.net/?retryWrites=true&w=majority`;
@@ -23,6 +30,28 @@ const client = new MongoClient(uri, {
   }
 });
 
+// middleware
+const logger = (req, res, next)=>{
+  console.log('log: info',req.method, req.url);
+  next();
+}
+
+const verifyToken = (req, res, next)=>{
+  const token = req?.cookies?.token;
+  // console.log('token in the middleware', token);
+  // if no token available
+  if(!token){
+    return res.status(401).send({message: 'Unauthorized Access'})
+  }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded)=>{
+    if(err){
+      return res.status(401).send({message: 'Unauthorized Access'});
+    }
+    req.user = decoded;
+    next()
+  })
+}
+
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
@@ -32,10 +61,24 @@ async function run() {
     const bookCollection = client.db("bookDB").collection("Book");
     const borrowBookCollection = client.db("bookDB").collection("borrowBook");
 
+    console.log(process.env.ACCESS_TOKEN_SECRET)
     // auth related api
-    app.post('/jwt', async(req, res)=>{
+    app.post('/jwt', logger, async(req, res)=>{
         const user = req.body;
         console.log('user for token', user)
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {expiresIn: '1h'});
+        res.cookie('token', token, { 
+          httpOnly: true,
+          secure: true,
+          sameSite: 'none'
+        })
+        .send({success: true})
+    })
+    
+    app.post('/logout', async(req, res) =>{
+      const user = req.body;
+      console.log('logging out', user)
+      res.clearCookie('token', {maxAge: 0}).send({success: true})
     })
 
 
@@ -48,8 +91,17 @@ async function run() {
       res.send(result);
     })
 
-    app.get('/borrowBook', async(req, res)=>{
-      const cursor = borrowBookCollection.find();
+    app.get('/borrowBook', logger, verifyToken, async(req, res)=>{
+      console.log(req.query.email);
+      console.log('token owner info', req.user)
+      if(req.user.email !== req.query.email){
+        return res.status(403).send({message: 'Forbidden Access'}) 
+      }
+      let query = {}
+      if(req.query?.email){
+        query = {email: req.query.email}
+      }
+      const cursor = borrowBookCollection.find(query);
       const result = await cursor.toArray()
       res.send(result);
     })
